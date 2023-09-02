44fc2650981df3c27f09ffc5d866e5fdb3eb0878
ElPsy <43912638+JhonnySalles@users.noreply.github.com>
2023-09-02 11:34:45 -0300

    Atualização de versão.

--- App/ExecutarOrdenaArquivo.bat
+++ App/ExecutarOrdenaArquivo.bat
@@ -1,1 +1,1 @@
-java -Dfile.encoding=utf-8 --add-opens java.base/java.lang.reflect=ALL-UNNAMED -jar ordenarArquivos-1.0.0-SNAPSHOT-jar-with-dependencies.jar
+java -Dfile.encoding=utf-8 --add-opens java.base/java.lang.reflect=ALL-UNNAMED -jar ordenarArquivos-1.0.1-SNAPSHOT-jar-with-dependencies.jar
