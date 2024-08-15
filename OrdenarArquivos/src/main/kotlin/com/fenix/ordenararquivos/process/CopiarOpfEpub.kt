99388f8cd4d5af0aa45acaeb68f89dbeaa18faef
ElPsy <jhonnysallesnoschag@hotmail.com>
2024-08-14 22:19:09 -0300

    Ajuste para inserção do opf no epub.

--- OrdenarArquivos/src/main/kotlin/com/fenix/ordenararquivos/process/CopiarOpfEpub.kt
+++ OrdenarArquivos/src/main/kotlin/com/fenix/ordenararquivos/process/CopiarOpfEpub.kt
@@ -51,7 +51,7 @@ object CopiarOpfEpub {
                 if (destino == null)
                     LOG.info("Arquivo de destino não encontrado.")
                 else {
-                    if (copiaOpf(origem, destino))
+                    if (copiaOpf(arquivo, destino))
                         LOG.info("Arquivo processado. Origem: " + origem.name + ", Destno: " + destino.name)
                     else
                         LOG.info("Arquivo não processado. Origem: " + origem.name + ", Destno: " + destino.name)
@@ -65,7 +65,7 @@ object CopiarOpfEpub {
         for (arquivo in destino.listFiles()!!) {
             if (arquivo.isDirectory)
                 arq = getDestino(arquivo, nome)
-            else if (arquivo.name == nome)
+            else if (arquivo.name.lowercase() == nome.lowercase())
                 arq = arquivo
 
             if (arq != null)
@@ -83,10 +83,13 @@ object CopiarOpfEpub {
         try {
             FiS = FileInputStream(origem)
             val ZiS = ZipInputStream(FiS)
-            var ZE: ZipEntry? = ZiS.getNextEntry()
+            var ZE: ZipEntry? = ZiS.nextEntry
             while (ZE != null) {
                 if (ZE.name.lowercase().endsWith(".opf")) {
-                    val opf = File(origem.parent + File.separator + ZE.name)
+                    val opf = File(origem.parent + File.separator + ZE.name.substringAfterLast('/'))
+                    if (opf.exists())
+                        opf.delete()
+
                     var FoS = FileOutputStream(opf)
                     var len: Int
                     while (ZiS.read(buffer).also { len = it } > 0) {
@@ -94,16 +97,10 @@ object CopiarOpfEpub {
                     }
                     FoS.close()
 
-                    FoS = FileOutputStream(destino)
-                    val zipOut = ZipOutputStream(FoS)
-                    val FiS = FileInputStream(opf)
-                    zipOut.putNextEntry(ZipEntry(ZE.name))
-                    val bytes = ByteArray(1024)
-                    var length: Int
-                    while (FiS.read(bytes).also { length = it } >= 0) {
-                        zipOut.write(bytes, 0, length)
-                    }
-                    FiS.close()
+                    compress(destino, FileInputStream(opf), ZipEntry(ZE.name))
+
+                    if (opf.exists())
+                        opf.delete()
                     processado = true
                     break
                 }
@@ -118,5 +115,49 @@ object CopiarOpfEpub {
             LOG.info("Erro ao processar o arquivo " + origem.name + ". Error: " + e.message)
         }
         return processado
+    }
+
+    private fun compress(zip: File, opf: FileInputStream, newEntry : ZipEntry) {
+        val tempFile = File.createTempFile(zip.name, null)
+        tempFile.delete()
+        zip.renameTo(tempFile)
+
+        val buf = ByteArray(1024)
+        val zin = ZipInputStream(FileInputStream(tempFile))
+        val out = ZipOutputStream(FileOutputStream(zip))
+
+        var locate = false
+        var entry = zin.nextEntry
+        while (entry != null) {
+            val name = entry.name
+            if (newEntry.name == name) {
+                out.putNextEntry(newEntry)
+                var len: Int
+                while (opf.read(buf).also { len = it } > 0) {
+                    out.write(buf, 0, len)
+                }
+                locate = true
+            } else {
+                out.putNextEntry(ZipEntry(name))
+                var len: Int
+                while (zin.read(buf).also { len = it } > 0) {
+                    out.write(buf, 0, len)
+                }
+            }
+            entry = zin.nextEntry
+        }
+
+        if (!locate) {
+            out.putNextEntry(newEntry)
+            var len: Int
+            while (opf.read(buf).also { len = it } > 0) {
+                out.write(buf, 0, len)
+            }
+        }
+
+        zin.close()
+        out.close()
+        tempFile.delete()
+        opf.close()
     }
 }
